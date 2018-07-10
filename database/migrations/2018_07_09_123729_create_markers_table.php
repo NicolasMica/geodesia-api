abc919b2e798435c73d9777da09afe31a3bdbc23
NicolasMica <nicolas@micallef.pro>
2018-07-10 15:00:54 +0200

    Laravel 5.6 upgrade

---
+++ database/migrations/2018_07_09_123729_create_markers_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMarkersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('markers', function (Blueprint $table) {
+
+            $table->increments('id');
+
+            $table->string('name');
+
+            $table->text('description');
+
+            $table->text('geometry');
+
+            $table->float('latitude');
+
+            $table->float('longitude');
+
+            $table->unsignedInteger('user_id')
+              ->foreign('user_id')
+              ->on('users');
+
+            $table->unsignedInteger('roadwork_id')
+              ->foreign('roadwork_id')
+              ->on('roadworks');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('markers');
+    }
+}
