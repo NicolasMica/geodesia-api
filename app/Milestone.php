753fefb3d557352b88d61ba667e33dd746e56ffb
NicolasMica <nicolas@micallef.pro>
2018-07-11 12:11:27 +0200

    Added laravel postgis support

--- app/Milestone.php
+++ app/Milestone.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Phaza\LaravelPostgis\Eloquent\PostgisTrait;
 
 class Milestone extends Model
 {
+    use PostgisTrait;
+
     /**
      * The connection name for the model.
      *
@@ -22,8 +25,29 @@ class Milestone extends Model
 
     /**
      * The primary key for the model.
-     * 
+     *
      * @var string
      */
     protected $primaryKey = 'gid';
+
+    /**
+     * Postgis specific fields
+     *
+     * @var array
+     */
+    protected $postgisFields = [
+        'geom'
+    ];
+
+    /**
+     * Casted postgis fields
+     *
+     * @var array
+     */
+    protected $postgisTypes = [
+        'geom' => [
+            'geomtype' => 'geometry',
+            'srid' => 3857 // 2153
+        ]
+    ];
 }
--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
          * Package Service Providers...
          */
         \Laravel\Passport\PassportServiceProvider::class,
+        Phaza\LaravelPostgis\DatabaseServiceProvider::class,
 
         /*
          * Application Service Providers...
